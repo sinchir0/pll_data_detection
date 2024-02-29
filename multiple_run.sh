90ca0ad66fbd2e84f8a25a70be4623d3448cf7c2
root <s-saito@chic.ocn.ne.jp>
2024-02-29 13:56:44 +0000

    add

--- multiple_run.sh
+++ multiple_run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd /notebooks
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp008.ipynb
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp009.ipynb
+# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp010.ipynb
+# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp011.ipynb
+# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp012.ipynb
+./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp013.ipynb
+# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp014.ipynb
+# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp015.ipynb
