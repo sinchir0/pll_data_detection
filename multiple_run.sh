142c63c269d56690554fa1af8d96454e66eced86
root <s-saito@chic.ocn.ne.jp>
2024-04-17 03:12:27 +0000

    add

--- multiple_run.sh
+++ multiple_run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp109.ipynb #exp097(exp072)のCV確認用
-# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp110.ipynb #exp096(exp080)のCV確認用
-# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp111.ipynb #exp086(exp083)のCV確認用
-
-# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp108.ipynb
-# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp098.ipynb
-# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp099.ipynb
-
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp113.ipynb
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp114.ipynb
-
+./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp120.ipynb
+./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp121.ipynb
+./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp122.ipynb
