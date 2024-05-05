4897c9ade3fd4607704bba62193d4252d42ff46b
sinchir0 <dekunattou@gmail.com>
2024-05-05 23:33:49 +0000

    add

--- multiple_run.sh
+++ multiple_run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp130.ipynb
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp131.ipynb
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp132.ipynb
-./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/exp133.ipynb
+# ./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/late09.ipynb
+./pll_data_detection/do_notebook_by_cli.sh pll_data_detection/exp/late10.ipynb
